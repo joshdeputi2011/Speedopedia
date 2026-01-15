cd099c446291f06a2878b38fc54999f28a6522b1
Josh Saju George <joshsaju2011@gmail.com>
2026-01-15 14:41:55 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/Speedopedia/', 
+  plugins: [
+    react(),
+    tailwindcss() // 🔴 THIS WAS MISSING
+  ],
+  base: '/Speedopedia/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
-  },// 🔴 REQUIRED for GitHub Pages
+  },
 })
-
