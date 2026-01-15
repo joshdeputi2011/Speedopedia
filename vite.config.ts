207ab0951aaa00296f565e91c4e7731f01ac1f1a
Josh Saju George <joshsaju2011@gmail.com>
2026-01-15 11:17:49 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Speedopedia/', // 🔴 MUST match repo name exactly
+  base: '/Speedopedia/', // 🔴 REQUIRED for GitHub Pages
 })
